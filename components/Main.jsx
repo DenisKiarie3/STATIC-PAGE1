b0744bcc833206d66fcf23440362eec0deb0004b
Denis Kiarie <deniskiarie3@gmail.com>
2025-04-08 19:08:09 +0300

    Moved background image to fit mobile screen

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,7 @@
 export default function Main () {
     return (
         <>
-            <main className="py-[60px] px-[30px] bg-[url('./images/react-logo.svg')] bg-no-repeat bg-[123%_70%]">
+            <main className="py-[60px] px-[30px] bg-[url('./images/react-logo.svg')] bg-no-repeat bg-[350%_70%]">
                 <h1 className="m-0 font-bold text-3xl">Fun facts about React</h1>
                     <ul className="mt-[35px] list-disc pl-5 marker:text-sky-400 marker:text-[1.3rem]">
                         <li className="py-[10px]">Was first released in 2013</li>
